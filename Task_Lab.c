b64963bd5601d2f3e8e6d99b8673892c113e05d9
Abdullah Mahmoud Abdul Latif <abdullah56mahmoud@gmail.com>
2022-10-13 17:18:40 +0200

    Task

---
+++ Task_Lab.c
@@ -0,0 +1,27 @@
+/*
+ *  Display the pattern like right angle triangle using a number.c
+ *
+ *  Created on: Oct 13, 2022
+ *      Author: Abdullah Mahmoud
+ */
+#include <stdio.h>
+int main()
+{
+	int x,y;
+
+	for(x=0;x<10;x++)
+	{
+		for(y=x;y<10;y++)
+		{
+			printf("%d",y);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+
+
